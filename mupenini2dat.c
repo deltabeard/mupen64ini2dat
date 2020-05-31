8fd15bdf186a979e2ec0db2b6b32d111e5c72476
Mahyar Koshkouei <mk@deltabeard.com>
2020-05-31 12:20:19 +0100

    Add storing of keys and CRC output
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- mupenini2dat.c
+++ mupenini2dat.c
@@ -14,22 +14,21 @@
  * THIS SOFTWARE.
  */
 
+#include <assert.h>
 #include <errno.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #define PRINTERR()	\
 	fprintf(stderr, "ERR %s:%d %s\n", __func__, __LINE__, strerror(errno))
 
-struct crc_entry_s
-{
-	uint64_t crc;
-};
-
 struct rom_entry_s
 {
+	uint64_t crc;
+
 	union
 	{
 		struct
@@ -64,8 +63,13 @@ struct rom_entry_s
 			unsigned char reference : 1;
 			uint16_t reference_entry;
 		};
-	};
-}__attribute__((packed));
+	} conf __attribute__((packed));
+
+	struct {
+		char md5[33];
+		char *cheat;
+	} track;
+};
 
 static char *read_entire_file(const char *filename)
 {
@@ -100,10 +104,13 @@ out:
 	return ini;
 }
 
-static size_t get_num_occs(char *haystack, const char *needle)
+/**
+ * Checks for number of occurrences of the word at the start of each line.
+ */
+static size_t get_num_occs(const char *haystack, const char *needle)
 {
 	size_t occurances = 0;
-	char *str = haystack;
+	char *str = strdup(haystack);
 	char *token = strtok(str, "\n");
 
 	do {
@@ -112,21 +119,269 @@ static size_t get_num_occs(char *haystack, const char *needle)
 	}
 	while((token = strtok(NULL, "\n")) != NULL);
 
+	free(str);
 	return occurances;
+}
+
+struct rom_entry_s *convert_entries(const char *ini,
+				    const size_t entries)
+{
+	#define strncmplim(hay, needle) strncmp(hay, needle, strlen(needle))
+	struct rom_entry_s *dat = calloc(entries, sizeof(struct rom_entry_s));
+	struct rom_entry_s *entry = dat;
+	int first = 1;
+	const char *line = ini;
+
+	if(dat == NULL)
+		goto out;
+
+	while((line = strchr(line, '\n')) != NULL)
+	{
+		line++;
+
+		/* Skip if empty line. */
+		if(*line == '\n')
+			continue;
+		/* Skip if comment. */
+		else if(*line == ';')
+			continue;
+		else if(strncmplim(line, "[") == 0)
+		{
+			line++;
+			strncpy(entry->track.md5, line, 32);
+			entry->track.md5[32] = '\0';
+		}
+		else if(strncmplim(line, "CRC") == 0)
+		{
+			uint32_t c1, c2;
+			char *endptr;
+			/* Compensate for 0-based indexing. */
+			if(first)
+				first = 0;
+			else
+			{
+				/* Set to new entry. */
+				entry++;
+			}
+
+			/* Move to first character after equals. */
+			line = strchr(line, '=');
+			assert(line != NULL);
+			line++;
+
+			c1 = strtoll(line, &endptr, 16);
+			assert(*endptr == ' ');
+
+			/* Move to second CRC. */
+			line = endptr;
+
+			c2 = strtoll(line, &endptr, 16);
+			assert(*endptr == '\n');
+			entry->crc = ((uint64_t)c1 << 32) | c2;
+		}
+		else if(strncmplim(line, "RefMD5") == 0)
+		{
+			line = strchr(line, '=') + 1;
+			entry->conf.reference = 1;
+
+			/* TODO: Find reference. */
+		}
+		else if(strncmplim(line, "SaveType") == 0)
+		{
+			line = strchr(line, '=') + 1;
+			switch(*line)
+			{
+			case 'E':
+				if(*(line + strlen("Eeprom ")) == '4')
+					entry->conf.save_type = 0;
+				else if(*(line + strlen("Eeprom ")) == '1')
+					entry->conf.save_type = 1;
+				else
+					abort();
+				break;
+			case 'S':
+				entry->conf.save_type = 2;
+				break;
+
+			case 'F':
+				entry->conf.save_type = 3;
+				break;
+
+			case 'C':
+				entry->conf.save_type = 4;
+				break;
+
+			case 'N':
+				entry->conf.save_type = 5;
+				break;
+
+			default:
+				abort();
+			}
+		}
+		else if(strncmplim(line, "Status") == 0)
+		{
+			char *endptr;
+			long int status;
+			line = strchr(line, '=') + 1;
+			status = strtol(line, &endptr, 10);
+			assert(*endptr == '\n');
+			assert(status < 6);
+			assert(status >= 0);
+			entry->conf.status = status;
+		}
+		else if(strncmplim(line, "Players") == 0)
+		{
+			char *endptr;
+			long int players;
+			line = strchr(line, '=') + 1;
+			players = strtol(line, &endptr, 10);
+			assert(*endptr == '\n');
+			assert(players < 8);
+			assert(players >= 0);
+			entry->conf.players = players;
+		}
+		else if(strncmplim(line, "Rumble") == 0)
+		{
+			line = strchr(line, '=') + 1;
+			entry->conf.rumble = (*line == 'Y');
+		}
+		else if(strncmplim(line, "CountPerOp") == 0)
+		{
+			char *endptr;
+			long int count_per_op;
+			line = strchr(line, '=') + 1;
+			count_per_op = strtol(line, &endptr, 10);
+			assert(*endptr == '\n');
+			assert(count_per_op <= 4);
+			assert(count_per_op > 0);
+			entry->conf.count_per_op = count_per_op;
+		}
+		else if(strncmplim(line, "DisableExtraMem") == 0)
+		{
+			line = strchr(line, '=') + 1;
+			entry->conf.count_per_op = (*line == '1');
+		}
+		else if(strncmplim(line, "Cheat0") == 0)
+		{
+			char *endline = strchr(line, '\n');
+			size_t len;
+			line = strchr(line, '=') + 1;
+
+			len = endline - line;
+			len++; /* For null char. */
+			entry->track.cheat = malloc(len);
+			assert(entry->track.cheat != NULL);
+			strncpy(entry->track.cheat, line, len);
+			entry->track.cheat[len - 1] = '\0';
+		}
+		else if(strncmplim(line, "Transferpak") == 0)
+		{
+			line = strchr(line, '=') + 1;
+			entry->conf.transferpak = (*line == 'Y');
+		}
+		else if(strncmplim(line, "Mempak") == 0)
+		{
+			line = strchr(line, '=') + 1;
+			entry->conf.biopak = (*line == 'Y');
+		}
+		else if(strncmplim(line, "Biopak") == 0)
+		{
+			line = strchr(line, '=') + 1;
+			entry->conf.biopak = (*line == 'Y');
+		}
+		else if(strncmplim(line, "SiDmaDuration") == 0)
+		{
+			line = strchr(line, '=') + 1;
+			assert(*line == '1');
+			entry->conf.si_dma_duration = 1;
+		}
+		else if(strncmplim(line, "GoodName") == 0)
+		{
+			/* Ignoring this key. */
+		}
+		else if(*line != '\0')
+		{
+			fprintf(stderr, "Unknown key %.32s\n", line);
+			abort();
+		}
+	}
+
+	/* All entries begin with a CRC value. */
+
+out:
+	return dat;
+}
+
+void dump_crc_header(const char *filename, struct rom_entry_s *e, size_t entries)
+{
+	FILE *f = fopen(filename, "wb");
+	time_t now = time(NULL);
+	struct tm *tmp;
+	char time_str[128];
+	assert(f != NULL);
+
+	tmp = localtime(&now);
+	assert(tmp != NULL);
+
+	if (strftime(time_str, sizeof(time_str), "%c", tmp) == 0)
+		abort();
+
+	fprintf(f, "/* Generated at %s using mupenini2dat */\n\n", time_str);
+	fprintf(f, "const uint64_t[%zu] = {\n\t", entries);
+
+	for(size_t i = 0; i < entries; i++)
+	{
+		if(i != 0 && i % 3 == 0)
+		{
+			fprintf(f, "\n\t");
+		}
+		else if(i != 0)
+		{
+			fprintf(f, " ");
+		}
+
+		fprintf(f, "0x%016lX%s", e[i].crc,
+			i == (entries-1) ? "" : ",");
+	}
+
+	fprintf(f, "\n}\n");
+	fclose(f);
+}
+
+int compare_entry(const void *in1, const void *in2)
+{
+	const struct rom_entry_s *e1 = in1;
+	const struct rom_entry_s *e2 = in2;
+
+	if(((__int128)e1->crc - (__int128)e2->crc) < 0)
+		return -1;
+
+	if(((__int128)e1->crc - (__int128)e2->crc) > 0)
+		return 1;
+
+	return 0;
+}
+
+void remove_dupes(struct rom_entry_s *all, size_t entries)
+{
+
 }
 
 int main(int argc, char *argv[])
 {
 	size_t entries;
 	char *ini;
+	struct rom_entry_s *all;
 
-	if(argc != 3)
+	if(argc != 4)
 	{
 		fprintf(stderr,
-			"mupenini2dat mupen64plus.ini mupen_rominfo.dat\n");
+			"mupenini2dat mupen64plus.ini rom.dat crc.h\n");
 		return EXIT_FAILURE;
 	}
 
+	/* Load ini file. */
 	ini = read_entire_file(argv[1]);
 	if(ini == NULL)
 		return EXIT_FAILURE;
@@ -135,9 +390,15 @@ int main(int argc, char *argv[])
 	 * must allocate. */
 	entries = get_num_occs(ini, "CRC=");
 
-	printf("Entries: %zu\n", entries);
-	/* Load ini file. */
+	printf("Processing %zu entries\n", entries);
+
+	all = convert_entries(ini, entries);
+	qsort(all, entries, sizeof(*all), compare_entry);
+	remove_dupes(all, entries);
+	dump_crc_header(argv[3], all, entries);
+
 	/* Read each entry. */
+
 	/* Sort ROM entries by CRC. */
 	/* Save cheat table,  */
 
